9ba790534dd55e85ea8cf3a6abe45f26f804ff82
Florian Bernd <git@flobernd.de>
2023-06-21 10:10:50 +0200

    Add support for query variables (#18)

--- src/datasource.ts
+++ src/datasource.ts
@@ -2,10 +2,12 @@ import { DataSourceInstanceSettings, CoreApp } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
 
 import { SnellerQuery, SnellerDataSourceOptions, DEFAULT_QUERY } from './types';
+import { SnellerVariableSupport } from "./variables";
 
 export class DataSource extends DataSourceWithBackend<SnellerQuery, SnellerDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<SnellerDataSourceOptions>) {
     super(instanceSettings);
+    this.variables = new SnellerVariableSupport()
   }
 
   getDefaultQuery(_: CoreApp): Partial<SnellerQuery> {
